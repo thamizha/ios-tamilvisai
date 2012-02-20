bdeb997ad5071833dcce8dda4a2e15a92422fbc3
Kishore Kumar <ukishore196@yahoo.co.in>
2012-02-20 21:32:48 +0530

    Initial import

---
+++ VisaiDemo/VisaiDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VisaiDemo
+//
+//  Created by Kishore Kumar on 20/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VisaiDemo/VisaiDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VisaiDemo
+//
+//  Created by Kishore Kumar on 20/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
